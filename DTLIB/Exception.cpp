a4c8a20f72b9b5d0b1198fb8edab466aafe1ffff
yhd_pc_windows_lab <1079420031@qq.com>
2020-09-14 14:29:36 +0800

    update Exception class

---
+++ DTLIB/Exception.cpp
@@ -0,0 +1,6 @@
+#include "Exception.h"
+
+Exception::Exception()
+{
+
+}
---
+++ DTLIB/Exception.h
@@ -0,0 +1,11 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+
+class Exception
+{
+public:
+    Exception();
+};
+
+#endif // EXCEPTION_H
