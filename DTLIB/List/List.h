dee42c7a9c4ca8e83f72b799a28fcc8b44859ed1
yhd_pc_windows_lab <1079420031@qq.com>
2020-09-21 10:09:15 +0800

    update

--- DTLIB/List/List.h
+++ DTLIB/List/List.h
@@ -8,7 +8,12 @@ namespace DTLib {
 template <typename T>
 class List : public Object
 {
+protected:
+    //Prohibit copy constructors and assignment overloaded functions
+    List(const List &);
+    List& operator = (const List &);
 public:
+    List() {};
     virtual bool insert(int i, const T& e) = 0;
     virtual bool remove(int i) = 0;
     virtual bool set(int i, const T& e) = 0;
--- DTLIB/main.cpp
+++ DTLIB/main.cpp
@@ -19,24 +19,12 @@ using namespace DTLib;
 
 int main(void)
 {
-    DynamicList<int> l(5);
+    StaticList<int, 5> s1;
 
-    for (int i = 0; i < l.capacity(); i++)
+    for (int i = 0; i < s1.capacity(); i++)
     {
-        l.insert(0, i);
+        s1[i] = i * i;
     }
-
-    for (int i = 0; i < l.length(); i++)
-    {
-        l[i] *= l[i];
-    }
-
-    for (int i = 0; i < l.length(); i++)
-    {
-        cout << l[i] << endl;
-    }
-
-
 
     return 0;
 }
