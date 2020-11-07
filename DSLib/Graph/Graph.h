e1da2194bb0450c64843122e8f2b93d944f80770
yhd_pc_windows_lab <1079420031@qq.com>
2020-11-07 21:52:09 +0800

    [add][Graph.h][class Graph]

---
+++ DSLib/Graph/Graph.h
@@ -0,0 +1,39 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "Object/Object.h"
+#include "SmartPointer/SharedPointer.h"
+#include "Array/DynamicArray.h"
+
+
+namespace DSLib {
+
+template <typename Vertex, typename Weight>
+class Graph : public Object
+{
+public:
+    virtual Vertex getVertex(int i) = 0;
+    virtual bool getVertex(int i, Vertex& Value) = 0;
+    virtual bool setVertex(int i, const Vertex& Value) = 0;
+    virtual SharedPointer< Array<int> > getAdjacent(int i) = 0;
+    virtual Weight getEdge(int i, int j) = 0;
+    virtual bool getEdge(int i, int j, Weight& value) = 0;
+    virtual bool setEdge(int i, int j, const Weight& value) = 0;
+    virtual bool removeEdge(int i, int j) = 0;
+    virtual int vertexCount() = 0;
+    virtual int edgeCount() = 0;
+    virtual int outDegree(int i) = 0;
+    virtual int inDegree(int i) = 0;
+
+    /********************************
+     *   the degree of the vertex
+     *******************************/
+    virtual int TDegree(int i)
+    {
+        return outDegree(i) + inDegree(i);
+    }
+};
+
+}
+
+#endif // GRAPH_H
